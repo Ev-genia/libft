311cc93ef41d2165761ad68b9880bd5d36e46805
Milagro Larra <mlarra@un-i2.kzn.21-school.ru>
2021-10-18 18:08:00 +0300

    add memcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mlarra <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 11:41:52 by mlarra            #+#    #+#             */
-/*   Updated: 2021/10/18 16:11:52 by mlarra           ###   ########.fr       */
+/*   Updated: 2021/10/18 16:51:44 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -31,4 +31,5 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
