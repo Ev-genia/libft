ec1e3346d6fb7d93771a28acf1463ff84d5a0cfe
Milagro Larra <mlarra@si-g1.kzn.21-school.ru>
2021-10-20 19:36:02 +0300

    change substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mlarra <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 19:32:21 by mlarra            #+#    #+#             */
-/*   Updated: 2021/10/20 11:24:14 by mlarra           ###   ########.fr       */
+/*   Updated: 2021/10/20 19:35:13 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dest;
 	size_t	i;
 
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s) - start;
 	dest = (char *)malloc(sizeof(char) * (len + 1));
 	if (!dest)
 		return (NULL);
