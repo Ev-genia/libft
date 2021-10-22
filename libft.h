ab1500b2b2d755d87efd7b0cac797d4db0816410
Milagro Larra <mlarra@va-c3.kzn.21-school.ru>
2021-10-22 15:00:30 +0300

    add striteri

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mlarra <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 11:41:52 by mlarra            #+#    #+#             */
-/*   Updated: 2021/10/22 12:31:05 by mlarra           ###   ########.fr       */
+/*   Updated: 2021/10/22 14:16:44 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -43,4 +43,5 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 #endif
